9edc25560ae585a63d20a2894e33d834553df807
bankinginverter <bankinginverter@gmail.com>
2022-07-19 23:31:57 +0700

    create lobby photon manager script

---
+++ Assets/U2T/Scripts/LobbyManager/LobbyPhotonManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class LobbyPhotonManager : MonoBehaviourPunCallbacks
+{
+    private RoomOptions _roomOptions;
+
+    private void Awake()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public void JoinOrCreateRoom(string roomName)
+    {
+        PhotonNetwork.JoinOrCreateRoom(roomName, _roomOptions,TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("");
+    }
+}
