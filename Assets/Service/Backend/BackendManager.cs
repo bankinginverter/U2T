8a899dfff5888edd0057d9c8ae9ac67baaf0c2b1
bankinginverter <bankinginverter@gmail.com>
2022-08-07 05:46:45 +0700

    add function remove data

--- Assets/Service/Backend/BackendManager.cs
+++ Assets/Service/Backend/BackendManager.cs
@@ -13,22 +13,28 @@ namespace U2T
         IMongoDatabase database;
         IMongoCollection<BsonDocument> collection;
 
-        private void Initialize()
+        public void Initialize()
         {
             mongoClient = new MongoClient("mongodb+srv://mndb1234:mndb1234@cluster0.njcbd.mongodb.net/testDB?retryWrites=true&w=majority");
             database = mongoClient.GetDatabase("mndb1234");
             collection = database.GetCollection<BsonDocument>("testDB");
         }
 
-        public void AddData(int id, bool status)
+        public void AddData(string username, string password)
         {
-            var document = new BsonDocument { { "id", id }, { "plant", status } };
+            var document = new BsonDocument { { "username", username }, { "password", password } };
             collection.InsertOne(document);
         }
 
-        private void GetData()
+        public void GetData()
         {
             collection.Find(new BsonDocument()).ForEachAsync(X => Debug.Log(X));
+        }
+
+        public void RemoveData(string username, string password)
+        {
+            var document = new BsonDocument { { "name", "bank" }};
+            collection.DeleteOne(document);
         }
     }
 }
