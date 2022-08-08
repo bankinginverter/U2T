98edbdf355dd2509afce9bb2657d529279c58fb7
bankinginverter <bankinginverter@gmail.com>
2022-08-08 20:59:11 +0700

    edit function EncodePasswordToHAS256

--- Assets/Service/Backend/BackendManager.cs
+++ Assets/Service/Backend/BackendManager.cs
@@ -1,5 +1,4 @@
-using System;
-using System.IO;
+using System.Text;
 using System.Security.Cryptography;
 using UnityEngine;
 using MongoDB.Driver;
@@ -23,7 +22,7 @@ namespace U2T
 
         public void AddData(string username, string password)
         {
-            var document = new BsonDocument { { "username", username }, { "password", EncodePassword(password)} };
+            var document = new BsonDocument { { "username", username }, { "password", EncodePasswordToHAS256(password)} };
             collection.InsertOne(document);
         }
 
@@ -38,48 +37,17 @@ namespace U2T
             collection.DeleteOne(document);
         }
 
-        private string EncodePassword(string password)
+        private string EncodePasswordToHAS256(string password)
         {
-            string encodePassword = "";
-            string directory = password;
-            if (Directory.Exists(directory))
+            var sha256 = SHA256.Create();
+            byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+            var stringbuilder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
             {
-                // Create a DirectoryInfo object representing the specified directory.
-                var dir = new DirectoryInfo(directory);
-                // Get the FileInfo objects for every file in the directory.
-                FileInfo[] files = dir.GetFiles();
-                // Initialize a SHA256 hash object.
-                using (SHA256 mySHA256 = SHA256.Create())
-                {
-                    // Compute and print the hash values for each file in directory.
-                    foreach (FileInfo fInfo in files)
-                    {
-                        using (FileStream fileStream = fInfo.Open(FileMode.Open))
-                        {
-                            try
-                            {
-                                // Create a fileStream for the file.
-                                // Be sure it's positioned to the beginning of the stream.
-                                fileStream.Position = 0;
-                                // Compute the hash of the fileStream.
-                                byte[] hashValue = mySHA256.ComputeHash(fileStream);
-                                // Write the name and hash value of the file to the console.
-                                Console.Write($"{fInfo.Name}: ");
-                                encodePassword += fInfo.Name;
-                            }
-                            catch (IOException e)
-                            {
-                                //Console.WriteLine($"I/O Exception: {e.Message}");
-                            }
-                            catch (UnauthorizedAccessException e)
-                            {
-                               //Console.WriteLine($"Access Exception: {e.Message}");
-                            }
-                        }
-                    }
-                }
+                stringbuilder.Append(bytes[i].ToString());
             }
-            return encodePassword;
+            return stringbuilder.ToString();
         }
     }
 }
