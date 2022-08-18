cfb1b61897875b4a7c1b7dc8af58427e6c7a7486
bankinginverter <bankinginverter@gmail.com>
2022-08-19 04:26:13 +0700

    fixed scale dialogBox prefab and refactor code

--- Assets/U2T/Scripts/LobbyManager/ChatMessage.cs
+++ Assets/U2T/Scripts/LobbyManager/ChatMessage.cs
@@ -6,70 +6,63 @@ using Photon.Pun;
 
 public class ChatMessage : MonoBehaviourPun
 {
-    [SerializeField] Transform parent;
+    [SerializeField] RectTransform parent;
 
     private PhotonView view;
     private InputField chatInputField;
     private Text updateText;
-
-    private bool send = false;
+    private int countMessage = 0;
 
     string currentText = "";
 
     private void Awake()
     {
         //instace = this;
+        //a = GetComponent<RectTransform>();
+        //parent.sizeDelta = new Vector2(10f,100f);
         view = GetComponent<PhotonView>();
-        //updateText = GameObject.Find("TextboxView").GetComponent<Text>();
         chatInputField = GameObject.Find("SendMessageBox").GetComponent<InputField>();
     }
 
     private void Update()
     {
-
         if (view.IsMine)
         {
-            if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
-            {
-
-                GameObject msgBox = Instantiate(Resources.Load("DialogBoxLocal") as GameObject);
-                msgBox.transform.parent = parent.transform;
-                msgBox.transform.GetChild(0).GetComponent<Text>().text = "  " + chatInputField.text;
-                currentText = chatInputField.text;
-                view.RPC("ISend", RpcTarget.Others, currentText);
-
-                //Debug.Log("Other");
-                //GameObject msgBoxOther = Instantiate(Resources.Load("DialogBoxOther") as GameObject);
-                //msgBoxOther.transform.parent = parent.transform;
-                //msgBoxOther.transform.GetChild(0).GetComponent<Text>().text = chatInputField.text + "  ";
-                //currentText = chatInputField.text;
-                //view.RPC("ISend", RpcTarget.All, currentText);
-                //send = false;
-
-                //updateText.text += chatInputField.text;
-                //currentText = updateText.text + "\n";
-                //view.RPC("ISend", RpcTarget.All, currentText);
-                //chatInputField.text = "";
-
-                send = false;
-            }
-            //if (send)
-            //{
-            //    GameObject msgBoxOther = Instantiate(Resources.Load("DialogBoxOther") as GameObject);
-            //    msgBoxOther.transform.parent = parent.transform;
-            //    msgBoxOther.transform.GetChild(0).GetComponent<Text>().text = chatInputField.text + "  ";
-            //    currentText = chatInputField.text;
-            //    view.RPC("ISend", RpcTarget.All, currentText);
-            //}
+            SendMessageToOtherPlayer();
         }
+        else
+        {
+            SendMessageToOtherPlayer();
+        }
+    }
+
+    private void SendMessageToOtherPlayer()
+    {
+        if ((Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter)) && chatInputField.text != "")
+        {
+            GameObject msgBox = Instantiate(Resources.Load("DialogBoxLocal") as GameObject);
+            msgBox.transform.parent = parent.transform;
+            msgBox.transform.localScale = new Vector3(1f,1f,1f);
+            msgBox.transform.GetChild(0).GetComponent<Text>().text = chatInputField.text + "  ";
+            currentText = chatInputField.text;
+            view.RPC("ISend", RpcTarget.Others, currentText);
+            chatInputField.text = "";
+            countMessage++;
+        }
+    }
+
+    private void ExplaneParentScale()
+    {
+
     }
 
     [PunRPC]
     void ISend(string message)
     {
-        message = chatInputField.text;
         GameObject msgBoxOther = Instantiate(Resources.Load("DialogBoxOther") as GameObject);
         msgBoxOther.transform.parent = parent.transform;
-        msgBoxOther.transform.GetChild(0).GetComponent<Text>().text = message + "  ";
+        msgBoxOther.transform.localScale = new Vector3(1f, 1f, 1f);
+        msgBoxOther.transform.GetChild(0).GetComponent<Text>().text = "  " + message;
+        countMessage++;
     }
 }
