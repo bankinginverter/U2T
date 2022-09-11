c4c0555d04c048190f07a4832730a486941e1110
bankinginverter <bankinginverter@gmail.com>
2022-09-11 17:33:00 +0700

    add function for clear all ui and ui list

--- Assets/U2T/Scripts/UIScripts/UIManagers.cs
+++ Assets/U2T/Scripts/UIScripts/UIManagers.cs
@@ -7,6 +7,7 @@ public class UIManagers : MonoBehaviour
 {
     public static UIManagers Instance;
 
+    private List<GameObject> UI = new List<GameObject>();
     private GameObject canvas;
 
     private void Awake()
@@ -19,11 +20,25 @@ public class UIManagers : MonoBehaviour
     {
         GameObject gameObjectUI = Instantiate(Resources.Load<GameObject>(nameUIPopup),canvas.transform) as GameObject;
         gameObjectUI.name = nameUIPopup;
+        UI.Add(gameObjectUI);
     }
 
     public void DisableUIPopUp(string nameUIPopup)
     {
         GameObject gameObjectUI = GameObject.Find(nameUIPopup).gameObject;
         Destroy(gameObjectUI);
+        UI.Clear();
+    }
+
+    public void DisableAllUIPopup()
+    {
+        foreach (var item in UI)
+        {
+            if (item.name != "GeneralMenuPopup")
+            {
+                Destroy(item);
+            }
+        }
+        UI.Clear();
     }
 }
