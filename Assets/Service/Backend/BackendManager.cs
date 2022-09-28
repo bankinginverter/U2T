824a1b856d32989b2e3bc4125744e8575b671987
bankinginverter <bankinginverter@gmail.com>
2022-09-29 03:45:58 +0700

    clear asset

--- Assets/Service/Backend/BackendManager.cs
+++ Assets/Service/Backend/BackendManager.cs
@@ -1,58 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
 using System.Text;
+using System;
+using System.Threading.Tasks;
 using System.Security.Cryptography;
 using UnityEngine;
-using MongoDB.Driver;
-using MongoDB.Bson;
-using MongoDB.Bson.IO;
-using System;
+using Proyecto26;
+using RSG;
 
-namespace U2T
+public class BackendManager : MonoBehaviour
 {
-    public class BackendManager
+    public string _databaseUrl;
+    User user;
+
+    private void Start()
     {
-        MongoClient mongoClient;
-        IMongoDatabase database;
-        IMongoCollection<BsonDocument> collection;
-
-        public void Initialize()
-        {         
-            mongoClient = new MongoClient("mongodb+srv://mndb1234:mndb1234@cluster0.njcbd.mongodb.net/testDB?retryWrites=true&w=majority");
-            database = mongoClient.GetDatabase("mndb1234");
-            collection = database.GetCollection<BsonDocument>("testDB");
-        }
-
-        public void AddData(string username, string password)
-        {
-            var document = new BsonDocument { { "username", username }, { "password", EncodePasswordToHAS256(password)} };
-            collection.InsertOne(document);
-        }
-
-
-        public BsonDocument FilterData(string fill, string value)
-        {
-            //collection.Find(new BsonDocument()).ForEachAsync(X => Debug.Log(X));   
-            var filter = Builders<BsonDocument>.Filter.Eq(fill, value);
-            var result = collection.Find(filter).FirstOrDefault();
-            return result;
-        }
-
-        public void RemoveData(string username, string password)
-        {
-            var document = new BsonDocument { { "name", "bank" }};
-            collection.DeleteOne(document);
-        }
-
-        public string EncodePasswordToHAS256(string password)
-        {
-            var sha256 = SHA256.Create();
-            byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
-
-            var stringbuilder = new StringBuilder();
-            for (int i = 0; i < bytes.Length; i++)
-            {
-                stringbuilder.Append(bytes[i].ToString());
-            }
-            return stringbuilder.ToString();
-        }
+        Initialize();
     }
+
+    public void Initialize()
+    {
+        _databaseUrl = "https://photharammetaverse-default-rtdb.asia-southeast1.firebasedatabase.app/";
+        user = new User();
+    }
+
+    public void saveDataToDatabase(string username, string password)
+    {
+        user = new User();
+        user.UserName = username;
+        string[] splitWord = user.UserName.Split('@');
+
+        RestClient.Put<User>(_databaseUrl + "/" + splitWord[0] + ".json", new User
+        {
+            UserName = username,
+            Password = password,
+        });
+    }
+
+    public async Task <string> ReadDataUsername(string username)
+    {
+        user = new User();
+        var a = RestClient.Get<User>(_databaseUrl + "/" + username + ".json").Then(response =>
+        {
+            user = response;
+        });
+        await Task.Delay(1000);
+        return user.UserName;
+    }
+
+    public async Task<string> ReadDataPassword(string username)
+    {
+        user = new User();
+        RestClient.Get<User>(_databaseUrl + "/" + username + ".json").Then(response =>
+        {
+            user = response;
+        });
+        await Task.Delay(1000);
+        return user.Password;
+    }
+
+    public string EncodePasswordToHAS256(string password)
+    {
+        var sha256 = SHA256.Create();
+        byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+        var stringbuilder = new StringBuilder();
+        for (int i = 0; i < bytes.Length; i++)
+        {
+            stringbuilder.Append(bytes[i].ToString());
+        }
+        return stringbuilder.ToString();
+    }
+}
+
+public class User
+{
+    public string UserName;
+    public string Password;
 }
